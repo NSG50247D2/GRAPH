925da5007b599e7ddb67befd60bf31a0d22ab51b
Himanshu Joshi <himanshujoshi5024@gmail.com>
2023-05-09 09:14:17 +0530

    Graph In DS

---
+++ Depth First Search(DFS).cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+using namespace std;
+
+typedef struct node
+{
+    int info;
+    struct node *next;
+}node;
+
+void readGraph(int v,node **adj_list)
+{
+    node *new_node,*last_node=NULL;
+    int info;
+    int k;
+    for(int i=0;i<v;i++)
+    {
+        cout<<"Enter number of nodes adjacent to "<<i<<" vertice : ";
+        cin>>k;
+        for(int j=1;j<=k;j++)
+        {
+            new_node=(node *)malloc(sizeof(node));
+            cout<<"Enter the "<<j<<" neighbour of "<<i<<" : ";
+            cin>>info;
+            new_node->info=info;
+            new_node->next=NULL;
+            if(adj_list[i]==NULL)
+            {
+                adj_list[i]=new_node;
+            }
+            else
+            {
+                last_node->next=new_node;
+            }
+            last_node=new_node;
+        }
+    }
+}
+
+void printGraph(int v,node **adj_list)
+{
+    for(int i=0;i<v;i++){
+        node *temp=adj_list[i];
+        if(temp!=NULL){
+            cout<<"Vertices adjacent to vertice "<<i<<" are: ";
+        }
+        else{
+            cout<<"Vertice "<<i<<" has no adjacency list.";
+        }
+        while(temp!=NULL){
+            cout<<temp->info<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+}
+
+void initializeVisited(int v,int *visited){
+    for(int i=0;i<v;i++){
+        visited[i]=0;
+    }
+}
+
+void DFS(int v,node **adj_list,int src,int visited[]){
+    visited[src]=1;
+    cout<<src<<" ";
+    node *temp=adj_list[src];
+    while(temp!=NULL){
+        if(visited[temp->info]!=1){
+            DFS(v,adj_list,temp->info,visited);
+        }
+        temp=temp->next;
+    }
+}
+
+int main(){
+    int v,ch,src;
+    cout<<"Enter number of vertices: ";
+    cin>>v;
+    int visited[v];
+    node *adj_list[v];
+
+    for(int i=0;i<v;i++){
+        adj_list[i]=NULL;
+    }
+
+    readGraph(v,adj_list);
+    do{
+        cout<<"Enter 1 for DFS, 2 for printing the graph, 3 for exit: ";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1: cout<<"Enter source node: ";
+                    cin>>src;
+                    initializeVisited(v,visited);
+                    DFS(v,adj_list,src,visited);
+                    break;
+            case 2: printGraph(v,adj_list);
+                break;
+            case 3: exit(0);
+            default: cout<<"Invalid argument!!"<<endl;
+        }
+    }while(ch);
+    return 0;
+}
